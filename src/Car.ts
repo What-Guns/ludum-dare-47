f995f7925179499f062af23faf36dc1f37fe7b5b
bassguitarbill <bassguitarbill@gmail.com>
2020-10-03 05:56:31 -0400

    Slight optimization

--- src/Car.ts
+++ src/Car.ts
@@ -31,7 +31,7 @@ export class Car{
   constructor(x:number, y: number) {
     this.x = x;
     this.y = y;
-    this.direction = Math.PI;
+    this.direction = 0;
   }
 
 
@@ -60,19 +60,20 @@ export class Car{
     const d = this.direction;
     const sin = Math.sin(d);
     const cos = Math.cos(d);
-    if(sin < Math.sin(Math.PI / 8) && sin > Math.sin(15 * Math.PI / 8)) {
+    const piOverEight = Math.PI / 8;
+    if(sin < Math.sin(piOverEight) && sin > Math.sin(15 * piOverEight)) {
       if(cos > 0) {
         return Car.IMAGES[0];
       } else {
         return Car.IMAGES[4];
       }
-    } else if(sin < Math.sin(3 * Math.PI / 8) && sin > 0) {
+    } else if(sin < Math.sin(3 * piOverEight) && sin > 0) {
       if(cos > 0) {
         return Car.IMAGES[7];
       } else {
         return Car.IMAGES[5];
       }
-    } else if(sin > Math.sin(11 * Math.PI / 8) && sin < 0) {
+    } else if(sin > Math.sin(11 * piOverEight) && sin < 0) {
       if(cos > 0) {
         return Car.IMAGES[1];
       } else {

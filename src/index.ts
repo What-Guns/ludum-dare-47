ccea5a2eb96fb6a3303a989ec6ee35db539f4769
Chris Mondok <chris.mondok@gmail.com>
2020-10-02 22:29:58 -0400

    Draw a map, like, right underneath bill's map

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {Game} from './Game.js';
+import {GameMap} from './Map.js';
 
 addEventListener('load', () => {
   startTheGameAlready()
@@ -7,23 +8,28 @@ addEventListener('load', () => {
 // any tick longer than this will be split into smaller ticks
 const BIG_TICK_ENERGY = 500;
 
-function startTheGameAlready() {
+async function startTheGameAlready() {
   const canvas = document.querySelector('canvas')!;
-  const ctx = canvas!.getContext('2d');
+  const ctx = canvas!.getContext('2d')!;
   const game = new Game(canvas);
+
+  const map = await GameMap.load('maps/map.json');
+
   requestAnimationFrame(tick);
 
   let lastTick = 0;
   function tick(timestamp: number) {
-    ctx?.clearRect(0, 0, 640, 640)
+    ctx.clearRect(0, 0, 640, 640)
     if(lastTick !== 0) {
       const dt = Math.min(timestamp - lastTick, BIG_TICK_ENERGY);
       game.tick(dt);
       game.draw(timestamp);
-      ctx!.fillStyle = "black";
-      ctx!.fillText(String(1000 / dt), 0, 50);
-      ctx!.fillText("Click to move the car!", 0, 100);
+      ctx.fillStyle = "black";
+      ctx.fillText(String(1000 / dt), 0, 50);
+      ctx.fillText("Click to move the car!", 0, 100);
     }
+
+    map.draw(ctx);
 
     lastTick = timestamp;
     requestAnimationFrame(tick);

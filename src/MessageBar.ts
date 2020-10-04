f68ecc994aae90a8867fbbfa79c8cf59e02b344a
bassguitarbill <bassguitarbill@gmail.com>
2020-10-04 08:02:42 -0400

    Funky text decryption

--- src/MessageBar.ts
+++ src/MessageBar.ts
@@ -2,7 +2,15 @@ export class MessageBar {
   private width = 0.8; // 80% of the screen
   private bottomMargin = 50;
   private height = 30; // 30 px tall
-  private messageText = 'WASD or Arrow Keys will let you drive around!';
+  private goalMessage = 'WASD or Arrow Keys will let you drive around!';
+  private messageText = this.gibberish(this.goalMessage.length);
+  private elapsedTime = 0;
+  private currentIterations = 0;
+
+  readonly maxIterations = 5;
+  readonly timeBetweenIterations = 180;
+  readonly unscramblePercentage = 0.6;
+
   draw(ctx: CanvasRenderingContext2D){
     ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
     const x = (1 - this.width) * ctx.canvas.width / 2;
@@ -16,5 +24,37 @@ export class MessageBar {
     ctx.fillText(this.messageText, ctx.canvas.width / 2, ctx.canvas.height - this.bottomMargin - 2, width);
   }
 
-  tick(){}
+  tick(dt: number){
+    this.elapsedTime += dt;
+    if(this.elapsedTime > this.timeBetweenIterations) {
+      this.elapsedTime -= this.timeBetweenIterations;
+      if (this.currentIterations < this.maxIterations) {
+        this.unscramblePartial();
+        this.currentIterations ++;
+      } else {
+        this.messageText = this.goalMessage;
+      }
+    }
+  }
+
+  setNewMessage(msg: string) {
+    this.goalMessage = msg;
+    this.messageText = this.gibberish(msg.length);
+    this.currentIterations = 0;
+  }
+
+  gibberish(length: number) {
+    // https://stackoverflow.com/a/12502559
+    let tempString = '';
+    do {
+      tempString += Math.random().toString(36).slice(2);
+    } while (tempString.length < length);
+    return tempString.slice(0, length);
+  }
+
+  unscramblePartial() {
+    for(let i=0; i<this.goalMessage.length; i++) {
+      if(Math.random() < this.unscramblePercentage) this.messageText = this.messageText.substring(0, i) + this.goalMessage.charAt(i) + this.messageText.substring(i+1);
+    }
+  }
 }

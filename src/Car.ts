6ebaf311a76f6d5bb5a990b6f74ad1fbe4391e4f
Peter David Zafonte <pzafonte@MacBook-Pro.local>
2020-10-05 17:37:16 -0400

    fix capactiy text

--- src/Car.ts
+++ src/Car.ts
@@ -371,7 +371,7 @@ export class Car extends GameObject {
   }
 
   denyPackage(){
-    game.hud.messageBar.setNewMessage("You require more car capacity. Drop off packages at to carry more");
+    game.hud.messageBar.setNewMessage("You require more car capacity. Drop off packages at to carry more!");
   }
 }
 

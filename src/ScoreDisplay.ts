25ce893380d8b34cde6c691d4f1b3bbb101ba304
bassguitarbill <bassguitarbill@gmail.com>
2020-10-05 16:50:14 -0400

    Score bigger!

--- src/ScoreDisplay.ts
+++ src/ScoreDisplay.ts
@@ -3,8 +3,11 @@ import {HUDElement} from './HUD.js';
 export class ScoreDisplay implements HUDElement {
   draw(ctx: CanvasRenderingContext2D): void {
     ctx.textAlign = 'right';
-    ctx.font = '24px KenneyMini';
+    ctx.font = '36px KenneyMini';
     ctx.fillStyle = 'black';
+    ctx.fillText(`$${game.gameInfo.score}`, ctx.canvas.width - 28, 52);
+    ctx.font = '36px KenneyMini';
+    ctx.fillStyle = 'green';
     ctx.fillText(`$${game.gameInfo.score}`, ctx.canvas.width - 30, 50);
   }
 
